d35f453f4a3312f185d2651181956fe107149a78
Jose Antonio Campos Gomez <jcamposjack@hotmail.com>
2022-05-09 11:39:41 +0200

    copy from private project

---
+++ angular-whishlist/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
+import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DestinoViajeComponent,
+    ListaDestinosComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-whishlist/src/app/models/destino-viaje.model.ts
@@ -0,0 +1,9 @@
+export class DestinoViaje{
+    nombre: string;
+    url: string;
+
+    constructor(nombre: string, url: string){
+        this.nombre = nombre;
+        this.url = url;
+    }
+}
